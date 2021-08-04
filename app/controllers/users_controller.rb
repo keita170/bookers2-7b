1fa1e33b279a06ddc9a5561e19c6433e624f58b4
keita170 <noinoi0565@gmail.com>
2021-08-04 06:04:06 +0000

    scope

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @book = Book.new
     @books = @user.books.order(:created_at)
+    
+    @today_book = @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
+    
   end
 
 
